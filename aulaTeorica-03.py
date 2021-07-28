416be79cd67f7132d6dd0e698c0c4d32f89a4a27
Matheus_SESH <matheusvinicius123466@gmail.com>
2021-07-28 10:20:26 -0300

    conditional exercise

---
+++ aulaTeorica-03.py
@@ -0,0 +1,17 @@
+# Condicional simples: 
+
+v1 = int(input("Digite um valor: "));
+v2 = int(input("Digite outro valor: "));
+
+if(v1 > v2):
+  print("O primeiro valor é maior que o segundo"); 
+
+# Condicional Composta: 
+
+v1 = int(input("Digite um valor: "))
+
+if(v1%2 == 0):
+  print("O valor é par")
+else:
+  print("O valor é impar")
+
